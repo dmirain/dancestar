59ec4d0520bdd7cacc129aa57b641e55c6100346
Dmitry Prokofyev <dmirain@yandex-team.ru>
2017-01-22 18:29:07 +0300

    remove folder

--- dancestar/settings.py
+++ dancestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(wcy)bhbrky80zo!4a5ke@2tcaj6c_g&zj2*lpda7ypg2+)=px'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
